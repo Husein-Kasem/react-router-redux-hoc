70604ad712bd46331a13800b576efd978df7bcb4
Husein Kasem <husein.kasem.90@gmail.com>
2018-11-26 22:05:09 +0100

    set up basic routing

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Home</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus eligendi magni quod explicabo asperiores unde pariatur corporis quibusdam nulla deleniti eum sequi, earum aliquid a quisquam cumque enim quia? Possimus.</p>
+    </div>
+  )
+}
+
+export default Home;
