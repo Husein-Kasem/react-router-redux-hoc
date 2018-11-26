e54108a39861a0a2ecee4f2d3de6cdd1e84690d5
Husein Kasem <husein.kasem.90@gmail.com>
2018-11-27 00:39:43 +0100

    added post component  and worked with router parameters

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,40 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from "react"
+import axios from "axios"
+import { Link } from "react-router-dom"
 
-import Preloader from "./Preloader";
+import Preloader from "./Preloader"
 
 class Home extends Component {
   state = {
     posts: []
-  };
+  }
 
   componentDidMount() {
     axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
       this.setState({
         posts: res.data.slice(0, 15)
-      });
-    });
+      })
+    })
   }
 
   render() {
-    const { posts } = this.state;
-    let postList = null;
+    const { posts } = this.state
+    let postList = null
     if (posts.length) {
       postList = posts.map(post => {
         return (
           <div className="post card" key={post.id}>
             <div className="card-content">
-              <span className="card-title">{post.title}</span>
+              <Link to={"/posts/" + post.id}>
+                <span className="card-title">{post.title}</span>
+              </Link>
               <p>{post.body}</p>
             </div>
           </div>
-        );
-      });
+        )
+      })
     } else {
-      postList = <Preloader />;
+      postList = <Preloader />
     }
 
     return (
@@ -39,8 +42,8 @@ class Home extends Component {
         <h4 className="center">Home</h4>
         {postList}
       </div>
-    );
+    )
   }
 }
 
-export default Home;
+export default Home
